fd81b80c881f925bf0b9bedcb620273eabf8a125
Krithika <goyalkrithika7@gmail.com>
2020-10-17 12:55:00 +0530

    dataCollection.py changed

--- dataCollection.py
+++ dataCollection.py
@@ -3,6 +3,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import TimeoutException
 import time
 import re,getpass,csv
 import datetime
@@ -15,17 +17,57 @@ def screenpos():
     time.sleep(1)
 
 PATH = "chromedriver"
-driver = webdriver.Chrome(PATH)
-driver.minimize_window()
-driver.implicitly_wait(1)
-while(1):
-    print("Enter the URL :-", end = ' ')
-    url = input()
+
+with open('urls.txt') as f:
+    lines = f.read().splitlines() 
+
+for url in lines:
+    driver = webdriver.Chrome(PATH)
+    driver.minimize_window()
+    driver.implicitly_wait(1)
     driver.get(url)
-    link = driver.find_elements_by_css_selector('a[class="link-gray-dark no-underline "]')[2]
-    contributor = WebDriverWait(driver,6,0.5).until(
-        EC.presence_of_element_located((By.LINK_TEXT, link.text)),
+    link = driver.find_element_by_css_selector('#js-repo-pjax-container > div.container-xl.clearfix.new-discussion-timeline.px-3.px-md-4.px-lg-5 > div > div.gutter-condensed.gutter-lg.flex-column.flex-md-row.d-flex > div.flex-shrink-0.col-12.col-md-3 > div')
+    link = link.find_element_by_partial_link_text('Contributors') #div.BorderGrid-cell > h2.h4 > a[class="link-gray-dark no-underline "]
+    print(link.text)
+    try:
+        repo_contributors = WebDriverWait(driver,6,0.5).until(
+            EC.presence_of_element_located((By.LINK_TEXT, link.text)),
+        )
+        repo_contributors.click()
+    except TimeoutException:
+        pass
+    num_contributors = WebDriverWait(driver,100).until(
+        EC.presence_of_all_elements_located((By.XPATH, '//*[@id="contributors"]/ol/li/span/h3/a[2]')),
     )
-    contributor.click()
-    driver.maximize_window()
-    screenpos()
+    contributors = []
+    repos = []
+    data = open("data.txt", 'a')
+    for x in range(len(num_contributors)): #len(contributor_repos)
+        data.write('\n')
+        path = '//*[@id="contributors"]/ol/li['+str(x+1)+']/span/h3/a[2]'
+        try:
+            contributor = WebDriverWait(driver,100).until(
+                EC.presence_of_element_located((By.XPATH, path)),
+            )
+            contributors.append(contributor.text)
+            data.write(contributor.text)
+            contributor.click()
+        except TimeoutException:
+            pass
+        user_repos = []
+        try:
+            user_repos = WebDriverWait(driver,5).until(
+                EC.presence_of_all_elements_located((By.CSS_SELECTOR, '#js-pjax-container > div.container-xl.px-3.px-md-4.px-lg-5 > div > div.flex-shrink-0.col-12.col-md-9.mb-4.mb-md-0 > div:nth-child(2) > div > div:nth-child(1) > div > ol > li > div > div > div > a > span')),
+            )
+            for repo in user_repos:
+                repos.append(repo.text)
+                data.write(" "+repo.text)
+
+        except TimeoutException:
+            pass
+                  
+        driver.back()
+        print(contributors)
+        print(repos)
+        time.sleep(3)
+    driver.quit()
